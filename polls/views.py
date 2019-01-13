fd52d79b39f9ccb7c91563bff04ea479b0f8925d
Olga <o.parfenova@gmail.com>
2019-01-13 18:49:53 +0300

    comment

---
+++ polls/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'polls/index.html')
+
+def register(request):
+    return render(request, 'polls/register.html')
+
+def login(request):
+    return render(request, 'polls/login.html')
