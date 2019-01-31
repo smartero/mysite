d7d4cad5ca848c724c3f54a1714af500d85c7eb5
Olga <o.parfenova@gmail.com>
2019-01-31 21:44:49 +0300

    edited result model

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models import DateTimeField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -16,17 +17,29 @@ class Car(models.Model):
     def __str__(self):
         return f'{self.your_car} {self.your_model}'
 
+class TripType(models.Model):
+    ONEWAY = 'OW'
+    ROUNDTRIP = 'RT'
+    TRIP_CHOICES = (
+        (ONEWAY, 'Oneway'),
+        (ROUNDTRIP, 'Roundtrip'),
+    )
+    choices = models.CharField(choices=TRIP_CHOICES, default=ONEWAY, max_length=2)
+    your_choice = models.CharField(max_length=50)
+
 class Result(models.Model):
+    
     title = models.CharField(max_length=100)
-    dep = models.CharField(max_length=100)
-    #date
-    arr = models.CharField(max_length=100)
+    trip_type = models.ForeignKey(TripType, blank=False, on_delete=models.CASCADE)
+    dep_city = models.CharField(max_length=100)
+    #dep_date = models.DateField(default=date.today)
+    resort = models.CharField(max_length=100)
     pax = models.IntegerField(default=1)
     baggage = models.IntegerField(default=0)
+    boots = models.IntegerField(default=0)
     car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='car')
     comment = models.TextField(max_length=300, default=None)
-    time = timezone.now()
+    time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.title}: {self.dep}, {self.arr}, {self.pax} {self.car}'
-
+        return f'{self.title}: {self.dep_city}, {self.resort}, {self.pax} {self.car}'
