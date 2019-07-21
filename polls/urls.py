561500d1854dcb9bb239c188307f157a336061c1
Olga <o.parfenova@gmail.com>
2019-07-21 22:31:20 +0300

    corrections

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView
 from polls import views as polls_views
-from  polls.views import SearchListView, ResultDeleteView
+from polls.views import SearchListView, ResultDeleteView
 
 urlpatterns = [
     path('', polls_views.index, name='index'),
